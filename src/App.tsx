49aef077ad031088de810a0765ebec95c4a85af3
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2026-07-30 13:26:04 +0700

    feat: Building a basic wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/Input";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Input id="name" label="Your Name" />
+      <Input id="age" label="Your Age" />
+    </main>
+  );
 }
 
 export default App;
