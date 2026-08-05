7ab7e734ae9ab8f8c416b41716c20f44d76a7ac5
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2026-08-05 18:14:21 +0700

    Building Another Wrapper Component (Custom Form Component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useRef } from "react";
+import Button from "./component/Button";
+import Form from "./component/Form";
 import Input from "./component/Input";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
---
+++ src/component/Form.tsx
@@ -0,0 +1,7 @@
+import type { ComponentPropsWithoutRef } from "react";
+
+type FormProps = ComponentPropsWithoutRef<"form">;
+
+export default function Form(props: FormProps) {
+  return <form {...props}>{props.children}</form>;
+}
