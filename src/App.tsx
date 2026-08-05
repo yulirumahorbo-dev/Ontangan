48c781ca818788de5c3dd70ff40376bc9089727d
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2026-08-05 20:16:37 +0700

    Exposing Component APIs with useImperativeHanle (with TypeScript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { useRef } from "react";
 import Button from "./component/Button";
-import Form from "./component/Form";
+import Form, { type FormHandle } from "./component/Form";
 import Input from "./component/Input";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
